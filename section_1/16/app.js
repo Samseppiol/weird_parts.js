4a16b7d961aa6aee5d26b49369451faef1878940
Samseppiol <j_hage@live.com>
2017-06-14 14:33:49 +1000

    Asynchronous callbacks

---
+++ section_1/16/app.js
@@ -0,0 +1,16 @@
+//  A long running function
+function waitThreeSeconds() {
+  var ms = 3000 + new Date().getTime();
+  while (new Date() < ms){}
+  console.log('finished')
+}
+
+
+function clickHandler() {
+  console.log('click event');
+}
+
+document.addEventListener('click', clickHandler);
+
+waitThreeSeconds();
+console.log('finished execution');
