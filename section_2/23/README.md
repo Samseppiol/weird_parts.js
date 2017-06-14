91d5512dd55e103fb24a3f774701f02998a155f5
Samseppiol <j_hage@live.com>
2017-06-15 08:22:16 +1000

    Existence with booleans

---
+++ section_2/23/README.md
@@ -0,0 +1,5 @@
+## Existence and Booleans
+#### Coercing with Booleans
+> Boolean(undefined) = false
+> Boolean(null) = false
+> Boolean("") = false
---
+++ section_2/23/app.js
@@ -0,0 +1,9 @@
+var a;
+
+// Anything put in an if as the statement will try to convert to a Boolean
+
+a = "Hello"
+// If 'a' is nothing then it will display false and not show anything to the console.
+if (a) {
+  console.log('Found something')
+}
