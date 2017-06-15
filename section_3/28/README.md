fa8b0fc4288bed48e94ef5513c2bec606a2e3b92
Samseppiol <j_hage@live.com>
2017-06-15 18:00:19 +1000

    Framework aside, faking namespaces

---
+++ section_3/28/README.md
@@ -0,0 +1,5 @@
+## Framework Aside, 
+
+### Faking Namespaces 
+> Container for variables and functions
+> To keep variables and functions with the same name separate.
---
+++ section_3/28/app.js
@@ -0,0 +1,14 @@
+var greet = "hello";
+var greet = "hola";
+
+// Hola will be run because it comes second, order of operations
+console.log(greet);
+
+var english = {};
+var spanish = {};
+
+
+english.greet = "Hello";
+spanish.greet = "hola!";
+
+console.log(english)
