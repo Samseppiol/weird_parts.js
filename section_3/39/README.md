fe2585a6ddd6c16efaf60e56e8d9fd12b2938de8
Samseppiol <j_hage@live.com>
2017-06-19 17:32:55 +1000

    Framework aside, whitespace

---
+++ section_3/39/README.md
@@ -0,0 +1,5 @@
+## Framework Aside,
+
+#### Whitespace
+> Invisible characters that create a literal 'space' within your code. 
+> E.g Carriage returns, tabs, spaces
---
+++ section_3/39/app.js
@@ -0,0 +1,10 @@
+var firstname, lastname, language;
+
+
+// Use as much whitespace as we want.
+var person = {
+    firstname: "Jack",
+    lastname: 'Doe'
+}
+
+console.log(person)
